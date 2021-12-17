ff3aebfdf3c98cf79ff06dc28ca8276ab0b26dd2
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-17 12:56:07 -0500

    6/12 inserts on PLANS table and separate .hbs section

--- src/database/DB.sql
+++ src/database/DB.sql
@@ -56,3 +56,82 @@ CREATE TABLE RESERVATIONS(
     CONSTRAINT fk_reservations_users FOREIGN KEY (usersId) REFERENCES USERS(usersId),
     CONSTRAINT fk_reservations_plans FOREIGN KEY (plansId) REFERENCES PLANS(plansId)
 );
+
+-- Initial inserts
+
+-- ######### PLANS ##########
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES("7 Canadian rivers on the road", 
+"Enjoy this plan in which you will travel all over Canada by road, visiting the 7 best-known rivers in the country. With this plan you will have the opportunity to have an encounter with nature while getting to know the culture of some Canadian cities.",
+"Canadá", 
+530.00, 
+"https://i.ibb.co/Wk9YTQq/canadian-rivers.jpg", 
+1, 
+1,
+1,
+1,
+7); 
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES("Ancient forest exploration", 
+"Enjoy the best natural experience you could imagine. Delve into the ancient forest and get to know its caves, rivers, waterfalls, animals and other natural environments. In addition to the above, you will live the experience of living in local cabins.", 
+"United States", 
+360.00, 
+"https://i.ibb.co/Mpc1qqc/ancient-forest.jpg", 
+1, 
+1,
+1,
+1,
+5); 
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES("Safari experience", 
+"Enjoy a guided experience in the bush savanna of Kenya. With this plan, you will have close experiences with the animals that inhabit the territory (don't worry, your safety is the most important thing for us). You will also live with locals in the area.", 
+"Kenya", 
+430.00, 
+"https://i.ibb.co/Z89qY6J/safari-experience.jpg", 
+1, 
+1,
+1,
+1,
+7); 
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES("Diving experience", 
+"Live the unique experience of diving in your own flesh, with this plan, you will be able to know aspects of the local culture of the country and immerse yourself in the ocean to know the marine flora and fauna of the place. Believe us, it will be an unforgettable experience.", 
+"Solomon Islands", 
+460.00, 
+"https://i.ibb.co/JnNkdqG/diving-experience.jpg", 
+1, 
+1,
+1,
+1,
+5); 
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES("Discover Perú", 
+"With this vacation plan, you will get to know the most important tourist sites in Peru, among which are Cuzco, Machu Picchu, the Vinicunca mountains and many more. You will also know first-hand the typical food of the country.", 
+"Perú", 
+310.00, 
+"https://i.ibb.co/pjzS8Wt/vinincunca-peru.jpg", 
+1, 
+1,
+1,
+2,
+7); 
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES("Cairo inmmersion", 
+"Meet the famous pyramids of Egypt! But it is not the only thing, with this plan you will also visit the recent National Museum of Egyptian Civilization, the Khan el-Khalili market, the citadel of Cairo, the Al-Azhar Mosque and some other important structures for Egyptian culture.", 
+"Egypt", 
+310.00, 
+"https://i.ibb.co/txcsBFv/cairo-inmmersion.jpg", 
+1, 
+1,
+1,
+1,
+7); 
+
+-- ########## ROLES ##########
+INSERT INTO ROLES (rolesDescription) VALUES ("Admin"); 
+INSERT INTO ROLES (rolesDescription) VALUES ("User"); 
