4f8dff274836cbbe782134edb56ec3d9d3a3ed6e
Silvia <silvia.pabon.2020@upb.edu.co>
2021-12-17 14:24:07 -0500

    Updating database, 6 more inserts
    -Ibiza
    -Blue Montains
    -Iceland
    -Tuscanye
    -Atacama
    -Manaus

--- src/database/DB.sql
+++ src/database/DB.sql
@@ -130,7 +130,79 @@ VALUES("Cairo inmmersion",
 1,
 1,
 1,
-7); 
+7);
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES('Ibiza Island vacation package',
+'In this plan you will stay in the beautiful Ibiza, visiting the incredible Passeig de ses Fonts and Eivissa Dalt Vila old town. With this plan you also will relax in the beach while enjoy the delicious mediterranean food.',
+'Spain',
+600.0,
+'https://i.ibb.co/HzpG5Jd/ibiza.jpg',
+1,
+1,
+1,
+2,
+7);
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES('Blue Mountains Park visit',
+'Learn why this amazing national park received this name by your own. With this plan you will visit in this World Heritage Area the Giant Stairway, Jenolan Caves and travel in the Katoomba Scenic Railway.',
+'Australia',
+550.0,
+'https://i.ibb.co/LrCw8Nb/blue-australia.jpg',
+1,
+1,
+1,
+3,
+6);
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES('Reykjavik Northen Lights tour',
+'If you enjoy cold water and have you ever dream with meet the amazing Northen lights, this is your plan. You can visit some places for shopping, hiking or listen a concert, like Harpa Reykjavík, Heiðmörk, Kringlan and Laugavegur according with your preferences',
+'Iceland',
+480.0,
+'https://i.ibb.co/Bn8hmN0/boreal-iceland.jpg',
+1,
+1,
+0,
+2,
+5);
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES('Tuscany roadtrip tasting wines',
+'Taste some of the best wines in the world through this road trip in Tuscany, visiting: Florence, Pontassieve, Poppi, Arezzo, Siena and Colle di Val d Elsa',
+'Italy',
+940.0,
+'https://i.ibb.co/5xjxs2y/toscane-italian.jpg',
+1,
+1,
+1,
+4,
+6);
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES('Atacama roadtrip',
+'Experience this incredible aventure in Atacama Desert, during the day you will visit some places like the Laguna Roja or the Salar of Atacama and during the night you will enjoy the best place on the planet to observe the sky',
+'Chile',
+400.0,
+'https://i.ibb.co/1zW6bZM/desert-chile.jpg',
+1,
+1,
+1,
+2,
+4);
+
+INSERT INTO PLANS (plansName, plansDescription, plansCountry, plansPrice, plansImageUrl, plansIncludeHotel, plansIncludeFlights, plansIncludeActivities, plansNumPersons, plansDays) 
+VALUES('Manaus jungle tour',
+'Enjoy this amazing excursion in the Manaus jungle, traveling through Amazon river, hiking and swimming to some precious cataracts, and you also will photograph some of the unique fauna and species of this zone',
+'Brazil',
+380.0,
+'https://i.ibb.co/P1h7ZvY/jungle-brazil.jpg',
+1,
+1,
+1,
+1,
+3);
 
 -- ########## ROLES ##########
 INSERT INTO ROLES (rolesDescription) VALUES ("Admin"); 
