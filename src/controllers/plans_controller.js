5189a921bda694f20351cb9a0da88d915f35cff9
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-18 10:18:13 -0500

    Plan details page initial version.
    Se agregó la ruta para mostrar la información de los diferentes planes.
    
    Cambios en:
    
    - plans_controller.js: El controlador se encarga de consultar la base de datos para obtener los datos del plan y los comentariod del plan.
    - index.js: Se agregaron los helpers de handdlebars para usar timeago.js
    - handlebarsHelpers.js: Importe de la librería timeago.js
    - _components.js: Se agregó un márgen superior e inferior en el .container, también se agregaron las clases .title y .grid_12.
    - main.scss: Responsive de la vista.
    - _planDetails.scss: Estilos principales de la vista.
    - plans.js: En la ruta /:id se importa el controlador.
    - planDetails.hbs: Se renderiza la información enviada desde el servidor.

--- src/controllers/plans_controller.js
+++ src/controllers/plans_controller.js
@@ -1,7 +1,48 @@
-const controller = {}; 
+const pool = require('../database/database');
+const controller = {};
 
 controller.index = (req, res) => {
-    res.send('This is the plans route'); 
+    res.send('This is the plans route');
 };
 
-module.exports = controller; 
+controller.specificPlan = async (req, res) => {
+    const { id } = req.params;
+
+    //Función para obtener los datos del plan y convertir los boleanos en strings
+    const obtainAndParseData = async (id) => {
+        const data = await pool.query('SELECT * FROM PLANS WHERE plansId = ?', [id]);
+
+        //Si no existe un plan con el id solicitado retorna null
+        if (Object.keys(data).length == 0) {
+            return null;
+        } else {
+            //Los booleanos los cambia por un string según el valor que tenga (true/false)
+            data[0].plansIncludeHotel = data[0].plansIncludeHotel == 1 ? 'Yes' : 'No';
+            data[0].plansIncludeFlights = data[0].plansIncludeFlights == 1 ? 'Yes' : 'No';
+            data[0].plansIncludeActivities = data[0].plansIncludeActivities == 1 ? 'Yes' : 'No';
+        }
+
+        return data;
+    };
+
+    const planData = await obtainAndParseData(id);
+
+    if (planData != null) {
+        //Si existe el plan, busca también los comentarios y envía toda la información a Handlebars
+        const planComments = await pool.query('SELECT * FROM COMMENTS WHERE plansId = ?', [id]);
+
+        //Objeto que handlebars renderizará
+        const handlebarsObject = {
+            title: 'Plan Details', //Título de la página
+            planData: planData[0], //Objeto que contiene la información del plan
+            planComments,
+        };
+
+        res.render('planDetails', handlebarsObject);
+    } else {
+        //SI no existió el plan con el ID solicitado, envía el siguiente string
+        res.send(`PLAN WITH ID ${id} DOESN´T EXIST`);
+    }
+};
+
+module.exports = controller;
