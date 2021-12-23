154d2ef5651e914dec364f51eafdcde9a0208eba
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-23 15:23:01 -0500

    Different navbar for users and visitors.
    La navbar para quienes no están logueados incluye:
    
    - Home, our plans, login, signup.
    
    La navbar para quienes sí están logueados incluye:
    
    - Home, dashboard, logout.
    
    IMPORTANTE:
    
    - Se agregó la ruta general /dashboard, de modo que a todos los usuarios se les redirige allí, pero si es admin, automáticamente se redirige a

--- src/routes/general_routes.js
+++ src/routes/general_routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database/database');
+const protectFunctions = require('../libs/protect_functions'); 
 
 router.get('/', async (req, res) => {
     //Antes de renderizar la vista, se cargan los planes para mostrarlos en el slider
@@ -15,5 +16,21 @@ router.get('/', async (req, res) => {
 
     res.render('index', handlebarsObject);
 });
+
+router.get('/dashboard', protectFunctions.isLoggedIn, async (req, res) => {
+    
+    /*Toma los roles del usuario*/
+    const id = req.user.usersId; 
+    const userRoles = await pool.query('SELECT roleId FROM usersHasRoles WHERE userId = ?', id); 
+
+    //Si tiene el rol de admin, lo redirige al dashboard de admin
+    if(userRoles.some( role => role.roleId == 1)){
+        res.redirect('/admin/dashboard');  
+    }else{
+        //Si no es admin, lo redirige al dashboard de user
+        res.redirect('/user/dashboard');  
+    }
+
+}); 
 
 module.exports = router;
