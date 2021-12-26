c85c6b57a9889a63a7223fa80f61122ea8baf252
Silvia <silvia.pabon.2020@upb.edu.co>
2021-12-25 22:38:50 -0500

    index route and controller for users_controller was not being used

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -1,10 +1,6 @@
 const controller = {}; 
 const passport = require('passport');
 
-controller.index = (req, res) => {
-    res.send('This is the user route'); 
-};
-
 controller.signup = (req, res) => {
     res.render('auth/signup');
 };
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/users_controller.js');
 
-router.get('/', controller.index);
-
 router.get('/signup', controller.signup);
 
 router.post('/signup', controller.signupPost);
