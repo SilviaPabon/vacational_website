cf3c47998cc2f23840556dd16c5e696d6e70c82a
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-26 11:39:45 -0500

    Indluce moment.js for TIMESTAMP conversion
    Se incluyó la librería moment.js para poder convertir la fecha de la BD a fecha local.

--- src/libs/handlebarsHelpers.js
+++ src/libs/handlebarsHelpers.js
@@ -1,8 +1,17 @@
 const { format } = require('timeago.js');
+const moment = require('moment'); 
+
 const helpers = {};
 
 helpers.timeago = (timestamp) => {
-    return format(timestamp);
+    //Adding french UTC to DB timestamp
+    const dbDate = moment(timestamp).utc().utcOffset(-5).format('MM/DD/YYYY, h:mm a'); 
+
+    //Convert french date to local
+    // IMPORTANT: Inside .utc() It is necessary to indicate the input format.
+    const localDate = moment.utc(dbDate, 'MM/DD/YYYY, h:mm a').local().format('MM/DD/YYYY, h:mm a'); 
+
+    return format(localDate);
 };
 
 module.exports = helpers;
